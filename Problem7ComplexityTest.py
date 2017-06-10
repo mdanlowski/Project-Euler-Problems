639687be1c714ea80026e2a5bc63fc51b12375f5
Martin <marcindanlowski@gmail.com>
2017-06-11 00:46:42 +0200

    Create Problem7ComplexityTest.py

--- Problem7ComplexityTest.py
+++ Problem7ComplexityTest.py
@@ -1,5 +1,7 @@
 # Problem 7 - 10001st prime
 """
+THIS FILE REQUIRES "primes.csv" in the directory
+
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10001st prime number?
 2 3 5 7 11 13 17 19 23 29 31 37 41
